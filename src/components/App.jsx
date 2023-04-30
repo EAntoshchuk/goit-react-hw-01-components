21302edcbabc3e0fff484a1dbf11bb9610ea8013
Eduard Antoshchuk <e.antoshchuk@gmail.com>
2023-04-30 16:05:58 +0300

    react-hw1-transactions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import user from '../json/user.json';
 import data from '../json/data.json';
 import friends from '../json/friends.json';
-// import transactions from '../json/transactions.json';
+import transactions from '../json/transactions.json';
 import Profile from './Profile';
 import FriendList from './FriendList';
 import Statistics from './Statistics';
+import TransactionHistory from './TransactionHistory';
 
 export default function App() {
   return (
@@ -19,16 +20,27 @@ export default function App() {
       }}
     >
       <span>React homework template</span>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <FriendList friends={friends} />
-      <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
+      <div>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </div>
+      <div>
+        <FriendList friends={friends} />
+      </div>
+      <div>
+        <Statistics title="Upload stats" stats={data} />
+      </div>
+      <div>
+        <Statistics stats={data} />
+      </div>
+      <div>
+        <TransactionHistory transactions={transactions} />;
+      </div>
     </div>
   );
 }
---
+++ src/components/TransactionHistory.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+
+export default function TransactionHistory({ transactions }) {
+  return (
+    <table class="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map(transaction => {
+          return (
+            <tr>
+              <td>{transaction.type}</td>
+              <td>{transaction.amount}</td>
+              <td>{transaction.currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
